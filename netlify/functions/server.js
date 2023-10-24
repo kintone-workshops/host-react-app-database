874e1f31562c70164019feaedd1d171dd7ad1686
sean-kintone <seanluse@cybozu.onmicrosoft.com>
2023-10-24 21:28:50 +0900

    edit server js

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,36 +1,28 @@
-// backend - server.js - Routes API requests from the frontend to Kintone
+// YOUR_BASE_DIRECTORY/netlify/functions/api.ts
 
-// Express Server Setup
-const express = require('express');
-const cors = require('cors');
-const PORT = 50000;
-const app = express();
-
-// Hide sensitive info in a .env file with dotenv
-require('dotenv').config({ path: '../.env' });
+import express, { Router } from "express";
+import serverless from "serverless-http";
 
 // Get Kintone credentials from a .env file
 const subdomain = process.env.SUBDOMAIN;
 const appID = process.env.APPID;
 const apiToken = process.env.APITOKEN;
 
-// Parse incoming requests with JSON payloads
-app.use(express.json());
-
-// Set Cross-Origin Resource Sharing (CORS) to frontend React App
-app.use(cors());
-const corsOptions = {
-  origin: 'http://localhost:3000'
-};
-
 // Kintone's record(s) endpoints
 const multipleRecordsEndpoint = `https://${subdomain}.kintone.com/k/v1/records.json?app=${appID}`
 const singleRecordEndpoint = `https://${subdomain}.kintone.com/k/v1/record.json?app=${appID}`;
-// Kintone app's field codes are 'country' 'state' and 'city'.
 
-// TODO: Create a GET endpoint at /getData
+
+const api = express();
+// Parse incoming requests with JSON payloads
+api.use(express.json());
+const router = Router();
+router.post("/postData", (req, res) => res.send("Hello World!"));
+
+api.use("/api/", router);
+
 // This route executes when a GET request lands on localhost:50000/getData
-app.get('/getData', cors(corsOptions), async (req, res) => {
+api.get('/getData', async (req, res) => {
   const fetchOptions = {
     method: 'GET',
     headers: {
@@ -44,7 +36,7 @@ app.get('/getData', cors(corsOptions), async (req, res) => {
 
 // TODO: Create a POST endpoint at /postData
 // This route executes when a POST request lands on localhost:50000/postData
-app.post('/postData', cors(corsOptions), async (req, res) => {
+api.post('/postData', async (req, res) => {
   const requestBody = {
     'app': appID,
     'record': {
@@ -72,6 +64,4 @@ app.post('/postData', cors(corsOptions), async (req, res) => {
   res.json(jsonResponse);
 });
 
-app.listen(PORT, () => {
-  console.log(`\n Backend server listening at http://localhost:${PORT} \n Confirm if Kintone records are being retrieved at \n http://localhost:${PORT}/getData`);
-});
+export const handler = serverless(api);
